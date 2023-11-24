f5c40c01d9282eca6c4824f389c90e3ec856c822
Anish.Joshi <anish.joshi@cedargate.com>
2023-11-24 18:32:29 +0545

    Added my portfolio into it

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/admin");
+  eleventyConfig.addPassthroughCopy("./src/mypic.jpg");
+  eleventyConfig.addPassthroughCopy("./src/codecareernepal.jpg");
+  eleventyConfig.addPassthroughCopy("./src/hamrocourse.jpg");
+  eleventyConfig.addPassthroughCopy("./src/hamrokhabar.jpg");
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
